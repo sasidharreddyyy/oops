86fda5038c91e781152bbb526d2eff739bd74aa4
sasidharreddyyy <sasidharreddy0112@gmail.com>
2025-03-10 16:33:11 +0530

    Add files via upload

---
+++ Exercise 1/SumOfTwoNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        scanner.close();
+
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+    }
+}
